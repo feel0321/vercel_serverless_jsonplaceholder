1c7e7fd5c9a1ef26aff38b300e21743b62f3bd35
고광필 <dishate@naver.com>
2022-07-17 04:15:38 +0900

    process.env 사용하도록 추가

--- api/[page].js
+++ api/[page].js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 // url/api/1 처럼 사용
 export default async function handler(request, response) {
   const { page } = request.query;
-  const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${page}`);
+  const res = await fetch(`${process.env.BASE_URL}/${page}`);
   const data = await res.json();
   return response.status(200).json({ data });
 }
--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 // url/api 처럼 사용
 export default async function handler(request, response) {
-  const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+  const res = await fetch(process.env.BASE_URL);
   const data = await res.json();
   return response.status(200).json({ data });
 }
