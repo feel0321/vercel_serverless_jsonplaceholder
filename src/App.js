b5a947fe3666053beaa6c83cd68c1def4c138318
고광필 <dishate@naver.com>
2022-07-17 02:54:42 +0900

    jsonplaceholder 요청 추가

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
+  return <div>{data?.title || '로딩 중'}</div>;
 }
 
 export default App;
